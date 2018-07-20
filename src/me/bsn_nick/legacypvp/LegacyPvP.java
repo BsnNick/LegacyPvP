f0bb91d51e87fa1118714e87b09dac32c548d44b
BsnNick <nick.b.321890@gmail.com>
2018-07-20 16:29:08 -0700

    Fix indentation

--- src/me/bsn_nick/legacypvp/LegacyPvP.java
+++ src/me/bsn_nick/legacypvp/LegacyPvP.java
@@ -17,47 +17,47 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class LegacyPvP extends JavaPlugin implements Listener
 {
-	private String name = this.getDescription().getName();
-	private String version = this.getDescription().getVersion();
-	private String author = this.getDescription().getAuthors().get(0);
-	
-	public void onEnable()
-	{
-		System.out.println("Enabling " + name + " " + version + " by " + author + "...");
-		
-		Bukkit.getPluginManager().registerEvents(this, this);
-		
-		System.out.println(name + " " + version + " by " + author + " has been enabled!");
-	}
-	
-	public void onDisable()
-	{
-		System.out.println(name + " " + version + " by " + author + " has been disabled!");
-	}
-	
-	@EventHandler
-	public void enableOldPvp(PlayerJoinEvent event)
-	{
-		try
-		{
-			event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).addModifier(new AttributeModifier("AttackSpeedRemover", 1.0, AttributeModifier.Operation.ADD_NUMBER));
-		}
-		catch (Exception e)
-		{
-			System.out.println("There was an error removing attack speed! Are you running Spigot 1.9 or above?");
-		}
-	}
-	
-	@EventHandler
-	public void reEnableForCompat(PlayerQuitEvent event)
-	{
-		try
-		{
-			event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).removeModifier(new AttributeModifier("AttackSpeedRemover", 1.0, AttributeModifier.Operation.ADD_NUMBER));
-		}
-		catch (Exception e)
-		{
-			System.out.println("There was an error removing attack speed! Are you running Spigot 1.9 or above?");
-		}
-	}
+    private String name = this.getDescription().getName();
+    private String version = this.getDescription().getVersion();
+    private String author = this.getDescription().getAuthors().get(0);
+    
+    public void onEnable()
+    {
+        System.out.println("Enabling " + name + " " + version + " by " + author + "...");
+        
+        Bukkit.getPluginManager().registerEvents(this, this);
+        
+        System.out.println(name + " " + version + " by " + author + " has been enabled!");
+    }
+    
+    public void onDisable()
+    {
+        System.out.println(name + " " + version + " by " + author + " has been disabled!");
+    }
+    
+    @EventHandler
+    public void enableOldPvp(PlayerJoinEvent event)
+    {
+        try
+        {
+            event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).addModifier(new AttributeModifier("AttackSpeedRemover", 1.0, AttributeModifier.Operation.ADD_NUMBER));
+        }
+        catch (Exception e)
+        {
+            System.out.println("There was an error removing attack speed! Are you running Spigot 1.9 or above?");
+        }
+    }
+    
+    @EventHandler
+    public void reEnableForCompat(PlayerQuitEvent event)
+    {
+        try
+        {
+            event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).removeModifier(new AttributeModifier("AttackSpeedRemover", 1.0, AttributeModifier.Operation.ADD_NUMBER));
+        }
+        catch (Exception e)
+        {
+            System.out.println("There was an error removing attack speed! Are you running Spigot 1.9 or above?");
+        }
+    }
 }
